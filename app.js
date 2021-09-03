852674d1f22c3d733a259eb9d427aa3e0be8319b
Lewis Oxley-Jenkins <lewis.oxley98@outlook.com>
2021-09-03 10:50:20 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const darkButton = document.getElementById('dark');
+const lightButton = document.getElementById('light');
+const solarButton = document.getElementById('solar');
+const body = document.body;
+
+const theme = localStorage.getItem('theme');
+const isSolar = localStorage.getItem('isSolar');
+
+if(theme) {
+    body.classList.add(theme);
+    isSolar && body.classList.add('solar')
+}
+
+darkButton.onclick = () => {
+    body.classList.replace('light', 'dark');
+    localStorage.setItem('theme', 'dark');
+};
+
+lightButton.onclick = () => {
+    body.classList.replace('dark', 'light');
+    localStorage.setItem('theme', 'light');
+
+    if(body.classList.contains('solar')) {
+        solarButton.style.cssText = `
+        --bg-solar: var(--blue);
+        `
+    }
+
+};
+
+solarButton.onclick = () => {
+    if (body.classList.contains('solar')) {
+        body.classList.remove('solar');
+        solarButton.style.cssText = `
+     --bg-solar: var(--yellow);
+    `
+    localStorage.removeItem('isSolar');
+
+    } else {
+        solarButton.style.cssText = `
+        --bg-solar: white;
+        `
+        body.classList.add('solar');
+        localStorage.setItem('isSolar', true);
+    }
+
+};
+
